64087ce808ba5f718f9b45a67dd7287b50149302
root <aurele284@gmail.com>
2024-10-21 12:00:13 +0000

    t6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,9 +13,7 @@ float c;
 printf("Size of char : %ld byte(s)\n", sizeof(b));
 printf("Size of int : %ld byte(s)\n", sizeof(a));
 printf("Size of long int : %lu byte(s)\n", sizeof(long int));
-printf("size of long long int : %lu byte(s)\n", sizeof(long int));
+printf("size of long long int : %lu byte(s)\n", sizeof(long long int));
 printf("Size of float : %ld byte(s)\n", sizeof(c));
-
-
 return (0);
 }
