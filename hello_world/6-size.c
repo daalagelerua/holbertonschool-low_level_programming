d832b39e660c09c412d22f9c300a797e8fbd3ac7
root <aurele284@gmail.com>
2024-10-21 11:40:33 +0000

    t6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,8 +9,10 @@ int main(void)
 {
 printf("Size of char : %ld byte(s)\n", sizeof(char *));
 printf("Size of int : %ld byte(s)\n", sizeof(int *));
+printf("Size of long int : %ld byte(s)\n", sizeof(long *));
+printf("size of long long int : %d byte(s)\n", sizeof(long long *));
 printf("Size of float : %ld byte(s)\n", sizeof(float *));
-printf("Size of double : %ld byte(s)\n", sizeof(double *));
+
 
 return (0);
 }
