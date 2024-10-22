77e9470948321cd4c9f101971354f30d7bc0daf1
root <aurele284@gmail.com>
2024-10-22 12:06:38 +0000

    t9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry
+* Return: 0
+*/
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+putchar(a + '0');
+if (a < 9)
+putchar(',');
+putchar(' ');
+putchar('\n');
+return (0);
+}
