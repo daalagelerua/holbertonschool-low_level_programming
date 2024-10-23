f61a5030fc345182ed581bee4ca14aeadbefc793
root <aurele284@gmail.com>
2024-10-23 08:50:19 +0000

    t2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet_x10 - entry
+*/
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			putchar(a);
+	}
+	putchar('\n');
+}
+
