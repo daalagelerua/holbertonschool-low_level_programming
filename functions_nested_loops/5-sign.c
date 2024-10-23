257e5d7bb2318ae378243d100703ba3d5ac7861a
root <aurele284@gmail.com>
2024-10-23 13:33:13 +0000

    t5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_sign - entry
+* Return: 1 0
+* @n: parameter
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+{
+		_putchar('+');
+		return (1);
+}
+	else if (n == 0)
+{
+		_putchar('0');
+		return (0);
+}
+	else
+{
+		_putchar('-')
+		return (-1)
+}
+}
