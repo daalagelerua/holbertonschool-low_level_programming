8dc8e17e40804528c8ba351a6f3dcb5adc115e76
root <aurele284@gmail.com>
2024-10-25 08:48:46 +0000

    t8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* jack_bauer - time of a day
+* description: 00:00 to 23:59
+*
+*/
+
+void jack_bauer(void)
+{
+int hh, mm;
+
+	for (hh = 0; hh < 24; hh++)
+{
+		for (mm = 0; mm < 60; mm++)
+{
+			_putchar(hh / 10 + '0');
+			_putchar(hh % 10 + '0');
+			_putchar(':');
+			_putchar(mm / 10 + '0');
+			_putchar(mm % 10 + '0');
+}
+}
+}
