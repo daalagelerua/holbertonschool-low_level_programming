85416edde3882faa5f45256f530aed3bbc532a26
root <aurele284@gmail.com>
2024-10-28 13:49:57 +0000

    t7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,13 +2,17 @@
 #include "main.h"
 
 /**
-*
-*
+* print_diagonal - entry
+* @n: para
 */
 
 void print_diagonal(int n)
 {
 int m, l;
+if (n < 0)
+_putchar('\n');
+else
+{
 for (l = 0; l < n; l++)
 {
 for (m = 0; m < l; m++)
@@ -17,5 +21,6 @@ _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
 }
