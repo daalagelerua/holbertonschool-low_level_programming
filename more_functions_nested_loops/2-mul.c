4762e1419d778e245f4a0eb541865e29f9cae825
root <aurele284@gmail.com>
2024-10-28 09:30:49 +0000

    t2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - entry
+* @a: para1
+* @b: para2
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+_putchar(a * b);
+return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
