4df5dc5270fdf81d9d3c8277fce7bad26f63fdb5
root <aurele284@gmail.com>
2024-10-28 15:13:11 +0000

    t10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,14 +12,12 @@ int a, b, c;
 if (size <= 0)
 _putchar('\n');
 else
-{
 for (a = 1; a <= size; a++)
 {
 for (b = 0; b < size - a; b++)
 _putchar(' ');
 for (c = 0; c < a; c++)
 _putchar('#');
-}
 _putchar('\n');
 }
 }
