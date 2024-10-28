dd230d2ebdeb629acdd7ccf050b0cc1f56288975
root <aurele284@gmail.com>
2024-10-28 09:10:09 +0000

    t0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isupper - entry
+* Return: 1 0
+* @c: para
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
