f918068404a71a77ef462d49b6c223facb4ab5e6
root <aurele284@gmail.com>
2024-10-28 13:10:56 +0000

    t6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -12,10 +12,10 @@ void print_line(int n)
 int m;
 
 if (n <= 0)
-_putchar('\n');
-for (m = 0; m < n; m++)
 {
-_putchar('_');
 _putchar('\n');
 }
+for (m = 0; m < n; m++)
+_putchar('_');
+_putchar('\n');
 }
