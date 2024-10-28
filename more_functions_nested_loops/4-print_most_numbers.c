3c5501ea493997232684e11c095b4b136fc144d3
root <aurele284@gmail.com>
2024-10-28 10:02:29 +0000

    t4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_most_numbers - entry
+*/
+
+void print_most_numbers(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+if (n != 2 && n != 4)
+_putchar(n);
+_putchar('\n');
+}
