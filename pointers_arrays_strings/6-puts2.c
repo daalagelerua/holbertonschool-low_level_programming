fc5a8293434d21f81c08a15787dcbc22eab46e69
root <aurele284@gmail.com>
2024-10-29 12:45:00 +0000

    t6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,13 @@ void puts2(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i += 2)
+{
 	if (str[i] == 'H' && i !=0)
+{
 	_putchar('\n');
 	return;
+}
 	_putchar(str[i]);
 _putchar('\n');
 }
-
+}
