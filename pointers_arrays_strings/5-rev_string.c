283012bc171976a02360adb3a7318140081ccad6
root <aurele284@gmail.com>
2024-10-29 10:29:04 +0000

    t5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* rev_string - entry
+* @s: ptr s
+*/
+
+void rev_string(char *s)
+{
+int str  = 0;
+int rev;
+while (s[str] != '\0')
+	str++;
+for (rev = 0; rev < str / 2; rev++)
+{
+	char tmp = s[rev];
+	s[rev] = s[str - 1 - rev];
+	s[str - 1 - rev] = tmp;
+}
+}
