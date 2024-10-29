94b534ed85a38022bd7daf1498c3ea7c205c43db
root <aurele284@gmail.com>
2024-10-29 13:23:40 +0000

    t7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts_half - entry
+* @str: ptr
+*/
+
+void puts_half(char *str)
+{
+int len = 0;
+int s, i;
+
+while (str[len] != '\0')
+len++;
+s = (len % 2 == 0) ? len / 2 : (len / 2) + 1;
+for (i = s; str[i] != '\0'; i++)
+_putchar(str[i];
+_putchar('\n');
+}
