2177ad9fd2aed14ea459e24c55d83c5020f65c98
root <aurele284@gmail.com>
2024-11-01 11:10:59 +0000

    t1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* *_strncat - entry
+* @dest: at the end of which src will be appended
+* @src: will be moved to the end of dest
+* @n: make sure src will be at most n bytes
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+char *ptr = dest;
+
+while (*ptr)
+ptr++;
+
+while (n > 0 && *src)
+{
+*ptr++ = *src++;
+n--;
+}
+*ptr = '\0';
+
+return (dest);
+}
