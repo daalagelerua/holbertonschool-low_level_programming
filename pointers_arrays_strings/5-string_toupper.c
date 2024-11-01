3ca7fddf4a857ccfccc79f996ed2da19033d633f
root <aurele284@gmail.com>
2024-11-01 14:29:05 +0000

    t5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* string_toupper - entry
+*Return: p
+*@s: para
+*/
+
+char *string_toupper(char *s)
+{
+char *p = s;
+
+while (*s)
+if (*s >= 'a' && *s <= 'z')
+*s -= 32;
+
+s++;
+
+return (p);
+}
