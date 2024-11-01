a2577efd3a188329adea9901392a6784caaae9e4
root <aurele284@gmail.com>
2024-11-01 15:17:09 +0000

    t6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,33 @@
 #include "main.h"
 
 /**
-*
-*
-*
+* cap_string - entry
+* @s: string
+* Return: s
 */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+int cap = 1;
+char *p = s;
+
+while (*p)
+{
+if (cap && *p >= 'a' && *p <= 'z')
+{
+*p = *p - ('a' - 'A');
+cap = 0;
+}
+else if (*p == ' ' || *p == '\t' || *p == '\n' || *p == '}' ||
+*p == ',' || *p == ';' || *p == '.' || *p == '!' ||
+*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{')
+cap = 1;
+else
+{
+if (*p >= 'A' && *p <= 'Z')
+cap = 0;
+}
+p++;
+}
+return (s);
+}
