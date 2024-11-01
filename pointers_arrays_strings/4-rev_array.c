3f072791c411ac7af389da2d6ea150c948e677b2
root <aurele284@gmail.com>
2024-11-01 13:13:01 +0000

    t4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*reverse_array - entry
+*@a: array
+*@n: content of array
+*@start: ptr to beginning of array
+*@end: ptr to end of array
+*/
+
+void reverse_array(int *a, int n)
+{
+int *start = a;
+int *end = a + n - 1;
+int tmp;
+
+while (start < end)
+{
+tmp = *start;
+*start = *end;
+*end = tmp;
+}
+start++;
+end++;
+}
