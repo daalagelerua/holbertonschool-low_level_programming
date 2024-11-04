0e65628af26d286872c264fc6ad7ac5e4f3121dc
root <aurele284@gmail.com>
2024-11-04 12:31:11 +0000

    t1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _memcpy - copie memory area
+* @dest: pointer to destination
+* @src: pointer to source
+* @n: number of bytes copied
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+
+return (dest);
+}
