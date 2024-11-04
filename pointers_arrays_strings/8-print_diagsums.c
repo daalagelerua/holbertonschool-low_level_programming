84a575070e3c4330d35242ee963d39cd5279e340
root <aurele284@gmail.com>
2024-11-04 16:20:42 +0000

    t7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagsums - print the sum of two diagonals
+* @a: array
+* @size: size of array
+*
+*/
+
+void print_diagsums(int *a, int size)
+{
+
