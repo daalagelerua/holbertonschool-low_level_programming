cabd9fd08f8b2ceccef726095783b8d5ff07d347
root <aurele284@gmail.com>
2024-11-04 13:14:42 +0000

    t2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_strchr - locate a character in a string
+* @s: string
+* @c: character to find
+* Return: NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (c == '\0')
+return s;
+return ('\0');
+}
