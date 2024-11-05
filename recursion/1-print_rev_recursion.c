9b88dc8e35f869636c403a9623d7b424acb185fe
root <aurele284@gmail.com>
2024-11-05 14:46:40 +0000

    t1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion - print string in reverse
+* @s: string
+* Return: if you reach end of line
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+return;
+
+_print_rev_recursion(s + 1);
+
+_putchar(*s);
+}
