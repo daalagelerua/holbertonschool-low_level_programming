b9b2ad47664be521d39d9e32e6b69ccf81353a30
root <aurele284@gmail.com>
2024-11-05 15:03:06 +0000

    t2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen_recursion - return length of string
+* @s: string
+* Return: 1 + len
+*
+*/
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+
+return 1 + _strlen_recursion(s + 1);
+}
+
+
+
