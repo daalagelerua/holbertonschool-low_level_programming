094feae994522a3db8889420342d7fe4862d7691
root <aurele284@gmail.com>
2024-11-06 14:00:00 +0000

    t0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - change name of programme
+* @argv: parameter
+* @argc: parameter
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+	printf("%s\n", argv[0]);
+
+return (0);
+}
+
