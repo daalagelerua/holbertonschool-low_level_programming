b2da5ebd0e466b871f4898981b824bf6cea13cd0
root <aurele284@gmail.com>
2024-11-06 10:28:20 +0000

    t5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,20 +2,34 @@
 #include "main.h"
 
 /**
+* _sqrt_recursion - return the natural sqrt
+* @n: number to find sqrt from
 *
+* _sqrt_helper - helper to find sqrt recursively
+* @guess: current guess of sqrt
 *
-*
+* Return: natural sqrt of n or -1 if no sqrt
 */
+
+int _sqrt_helper(int n, int guess);
 
 int _sqrt_recursion(int n)
 {
-int i;
-
 if (n < 0)
 	return (-1);
 
-if (i * i > n)
+return (_sqrt_helper(n, 0));
+/* 0 is the initial guessfor the sqrt*/
+}
+
+int _sqrt_helper(int n, int guess)
+{
+if (guess * guess > n)
 	return (-1);
-
-
-return (_sqrt_recursion + 1);
+/* it doesn't find a natural sqrt*/
+if (guess * guess == n)
+	return (guess);
+/* that guess is the natural sqrt */
+return _sqrt_helper(n, guess + 1);
+/* try the next number */
+}
