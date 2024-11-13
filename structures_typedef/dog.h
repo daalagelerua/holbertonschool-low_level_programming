18d391a6553d72174122a44d09def7f79ddf7299
daalagelerua <aurele284@gmail.com>
2024-11-13 20:51:31 +0000

    t0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HOME_STRUCT
+#define HOME_STRUCT
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct dog - structure storing dog info
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
---
+++ structures_typedef/main.h
@@ -0,0 +1,11 @@
+#ifndef HOME_STRUCT
+#define HOME_STRUCT
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+#endif
