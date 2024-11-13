95bd150dc2f486bac89b2ceff4beaff52da93312
daalagelerua <aurele284@gmail.com>
2024-11-13 10:26:49 +0000

    t3 projet 0 update

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int i, j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 
-grid = malloc(sizeof(int*) * height);
+grid = malloc(sizeof(int *) * height);
 
 if (grid == NULL)
 	return (NULL);
@@ -28,16 +28,17 @@ for (i = 0; i < height; i++)
 	{
 	grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
-		{
-		for (j = 0; j < i; j++)
 			{
-			free(grid[j]);
-			}
+			while (i > 0)
+				{
+				i--;
+				free(grid[i]);
+				}
 			free(grid);
 			return (NULL);
 			}
-for (j = 0; j < width; j++)
-	grid[i][j] = 0;
+	for (j = 0; j < width; j++)
+		grid[i][j] = 0;
 	}
 return (grid);
 }
