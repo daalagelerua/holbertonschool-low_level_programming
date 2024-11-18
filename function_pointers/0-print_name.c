4fb93e0be01402683c48dd3ca73111f827e4f011
daalagelerua <aurele284@gmail.com>
2024-11-18 12:45:42 +0000

    t0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* print_name - print a name
+* @name: string
+* @f: pointer to other function
+*
+*/
+
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
