b3e20b030594f3f3444a36be3a8727455e370d5f
daalagelerua <aurele284@gmail.com>
2024-11-18 13:52:34 +0000

    t0 correct

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,11 +5,11 @@
 * print_name - print a name
 * @name: string
 * @f: pointer to other function
-*
 */
 
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL && f == NULL)
 f(name);
 }
