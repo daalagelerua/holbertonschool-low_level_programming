a484c3273d88c31fb4e0965019820883b0755d1f
daalagelerua <aurele284@gmail.com>
2024-11-18 13:54:04 +0000

    0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef HOME_FUNC_POINTERS
-#define HOME_FUNC_POINTERS
+#ifndef HOME_FUNC_POINTERS_H
+#define HOME_FUNC_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
