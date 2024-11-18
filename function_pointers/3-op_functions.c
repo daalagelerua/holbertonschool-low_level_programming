5c946ee2c52d14075023250c92f0d6a0dd65dea7
daalagelerua <aurele284@gmail.com>
2024-11-18 15:13:15 +0000

    t3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,10 +1,70 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+/**
+* op_add - Returns the sum of two integers.
+* @a: First integer.
+* @b: Second integer.
+* Return: sum
+*/
 
-#endif
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - Returns the difference of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Difference of a and b.
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - Returns the product of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Product of a and b.
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - Returns the result of the division of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Quotient of a and b.
+*/
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0); /* Division by zero is handled in main */
+	}
+	return (a / b);
+}
+
+/**
+* op_mod - Returns the remainder of the division of two integers.
+* @a: First integer.
+* @b: Second integer.
+*
+* Return: Remainder of a divided by b.
+*/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0); /* Division by zero is handled in main */
+	}
+	return (a % b);
+}
