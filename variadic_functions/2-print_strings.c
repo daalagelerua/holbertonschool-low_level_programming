760f56b1b8836d14a3bb0d90dda0a1ebeda0e68d
daalagelerua <aurele284@gmail.com>
2024-11-21 13:37:46 +0000

    t2 first try

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,14 +1,39 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
-*
-*
-*
-*
-*
+* print_strings - prints strings followed by new line
+* @separator: coma to be printed between strings
+* @n: number of strings passed to the function
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+unsigned int i;
+va_list arg;
+char *str;
 
+if (n == 0)
+	{
+	printf("\n");
+	return;
+	}
+va_start(arg, n);
+
+for (i = 0; i < n; i++)
+	{
+	str = va_arg(arg, char *);
+		{
+		if (str == NULL)
+		printf("(nil)");
+		else
+		printf("%s", str);
+		}
+
+	if (separator != NULL && i < n - 1)
+		printf("%s", separator);
+	}
+va_end(arg);
+printf("\n");
 }
