a447a0a6a136d892e548fbf618a47ac3067b031e
daalagelerua <aurele284@gmail.com>
2024-11-21 15:41:05 +0000

    t3 ajout d'une struct

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdio.h>
 
+typedef struct
+{
+	char * spec;
+	void (*print_func)(char *):
+}print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
