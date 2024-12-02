7dc9bdd18e3ffd042d3a05c1cffc9767606c9215
daalagelerua <aurele284@gmail.com>
2024-12-02 13:18:43 +0000

    function print_list good output

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,29 @@
 #include "lists.h"
 
 /**
-*
-*
-*
+* print_list - prints all elements of list_t list
+* @h: ptr to first node
+* Return: number of nodes
 */
 
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
+const list_t *current = h;
+size_t count = 0;
 
+while (current != NULL)
+	{
+	if (current->str == NULL)
+		{
+		printf("[0] (nil)\n");
+		}
+	else
+		{
+		printf("[%d] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		count++;
+	}
+return (count);
 }
