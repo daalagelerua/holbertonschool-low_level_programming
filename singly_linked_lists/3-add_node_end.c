09e58fc7e129ac36bc514a47883e39c0e7098bc7
daalagelerua <aurele284@gmail.com>
2024-12-03 12:04:49 +0000

    modification du code function add_node_end good output

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,31 +9,30 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
-list_t *temp;
+list_t *new; /*nouveau noeud*/
+list_t *temp; /*pointeur temporaire pour parcourir la liste*/
 
-if (head == NULL)
-	{
-	new = malloc(sizeof(list_t));
-	new = *head;
-	}
-else
-{
-temp = *head;
-while (temp->next != NULL)
-	{
-	*head++;
-	temp++;
-	}
-new = malloc(sizeof(list_t));
+if (head == NULL || str == NULL) /*verifier si la liste est vide*/
+	return (NULL);
+
+new = malloc(sizeof(list_t)); /*allouer memoire pour nouveau noeud*/
 	if (new == NULL)
 		return (NULL);
-	else
-		{
-		new->str = strdup;
-		new->len = strlen;
-		new->next = NULL;
-		temp->next = new;
-		}
+
+new->str = strdup(str); /*copie la chaine*/
+new->len = strlen(str); /*calcul sa longueur*/
+new->next = NULL; /*le nouveau noeud sera le dernier*/
+
+if (*head == NULL) /*si la liste est vide le nouveau noeud devient la tete*/
+	{
+	*head = new;
+	return (new);
+	}
+temp = *head;
+while (temp->next != NULL) /*parcourir la liste jusqu'au dernier noeud*/
+	temp = temp->next;
+
+temp->next = new; /*ajouter le nouveau noeud a la fin*/
+
 return (new);
 }
