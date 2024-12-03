5a8d73a86de8654ae20f61d936ada7d923463dd9
daalagelerua <aurele284@gmail.com>
2024-12-03 16:04:19 +0000

    function print_list good output

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,11 +2,19 @@
 
 /**
 * print_dlistint - prints all element of a dlistint list
-* @h: pointer to first element
+* @h: const pointer to first node
 * Return: number of nodes
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
+size_t count = 0; /*node counter*/
 
+while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
+	}
+return (count);
 }
