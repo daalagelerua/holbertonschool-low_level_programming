7253183bd1d27cdceda19cb0fccd8604fa032307
daalagelerua <aurele284@gmail.com>
2024-12-03 16:31:22 +0000

    function dlistint_len good output

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -12,6 +12,7 @@
  * Description: doubly linked list node structure
  *
  */
+
 typedef struct dlistint_s
 {
     int n;
@@ -20,7 +21,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
 
 
 
