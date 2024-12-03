0df3a2a790539677f2e600b1676d459c9d4e0599
daalagelerua <aurele284@gmail.com>
2024-12-03 15:44:25 +0000

    creation dossier + header + readme

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all element of a dlistint list
+* @h: pointer to first element
+* Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
