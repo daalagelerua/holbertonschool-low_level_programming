4fc91258e12048839d41520595fba9dc9737f79c
daalagelerua <aurele284@gmail.com>
2024-12-04 13:44:54 +0000

    function delete_dnodeint good output

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,18 +9,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *temp;
+dlistint_t *temp = *head;
 unsigned int i;
-
-temp = *head;
-for (i = 0; temp != NULL && i < index; i++)
-	temp = temp->next;
-
-if (temp == NULL)
+if (*head == NULL) /*la liste est vide, rien a supprimer*/
 	return (-1);
 
-temp->next->prev = temp->prev;
-temp->prev->next = temp->next;
+if (index == 0) /*le node a suppr est la head*/
+	{
+	*head = temp->next; /*MAJ de head pour qu'il pointe sur next node*/
+	if (*head != NULL) /*si le next node existe*/
+		(*head)->prev = NULL; /*son prev devient NULL parce qu'il devient 1er*/
+	free(temp); /*free l'ancien head*/
+	return (1);
+	}
+
+/*si node au milieu*/
+for (i = 0; temp != NULL && i < index; i++) /*parcourir jusqu'a l'index*/
+	temp = temp->next;
+
+if (temp == NULL) /*si index trop grand*/
+	return (-1);
+
+if (temp->next != NULL) /*si le noeud a un suivant*/
+	temp->next->prev = temp->prev; /*prev du next node devient prev de temp*/
+if (temp->prev != NULL) /*si le noeud a un predent*/
+	temp->prev->next = temp->next; /*next du prev node devient next de temp*/
+
 free(temp);
 
 return (1);
