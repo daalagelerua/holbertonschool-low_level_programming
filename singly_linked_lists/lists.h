2ab2c9df502388b2dd14be8b36887b947c844b9d
daalagelerua <aurele284@gmail.com>
2024-12-05 14:53:53 +0000

    delete space from struct

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,11 +13,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
