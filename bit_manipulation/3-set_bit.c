0fe0981f8aac949d84f75e6fe88af0f296600e20
daalagelerua <aurele284@gmail.com>
2024-12-06 12:42:24 +0000

    delete space for my dear betty

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* set-bit - sets the value of a bit to 1 at a given index
+* set_bit - sets the value of a bit to 1 at a given index
 * @n: pointer to the unsigned long integer whose bit is to be set
 * @index: index starting from 0 of the bit you want to set
 * Return: 1 if works or -1 if error
