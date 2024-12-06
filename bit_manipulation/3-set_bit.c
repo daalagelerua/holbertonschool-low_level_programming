dd9d85dcd53084a70b2516a486c713fbed7bdd03
daalagelerua <aurele284@gmail.com>
2024-12-06 12:41:34 +0000

    delete space for my dear betty

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int mask = 0;
 if (index > 63)
- 	return (-1);
+	return (-1);
 mask = 1 << index;
 *n = *n | mask;
 	return (1);
