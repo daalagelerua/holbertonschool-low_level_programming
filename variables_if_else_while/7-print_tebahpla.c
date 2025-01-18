2d0e496d280ad1e153fe264068f771d2088dd4bc
Aurèle Perillat <aurele284@gmail.com>
2025-01-18 14:39:49 +0100

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - entry
+* main - print alphabet in reverse
 * Return: 0
 */
 int main(void)
 {
 char a;
-for (a = 'z'; a >= 'a'; a--)
-putchar(a);
-putchar('\n');
+
+    for (a = 'z'; a >= 'a'; a--)
+        putchar(a);
+
+    putchar('\n');
+
 return (0);
 }
