708a739f20dd14f3c458e087862261d9a5c16b68
Aurèle Perillat <aurele284@gmail.com>
2025-01-18 14:38:25 +0100

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - entry
+* main - print numbers from 0 to 9
 * Return: 0
 */
 int main(void)
 {
 int a;
-for (a = 0; a <= 9; a++)
-putchar(a + '0');
-putchar('\n');
+
+    for (a = 0; a <= 9; a++)
+        putchar(a + '0');
+
+    putchar('\n');
+
 return (0);
 }
