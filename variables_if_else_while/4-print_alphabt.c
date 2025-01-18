2d3e2ef679fd3119a40fda4986c26110272297f3
Aurèle Perillat <aurele284@gmail.com>
2025-01-18 14:25:53 +0100

    add comments

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print alphabet wthout e or q
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int a;
 
-for (a = 'a'; a <= 'z'; a++)
-{
-if (a == 'e' || a == 'q')
-continue;
-putchar(a);
-}
-putchar('\n');
+    for (a = 'a'; a <= 'z'; a++) /* iterate through each letter of the alphabet */
+    {
+        if (a == 'e' || a == 'q') /* if letter is e or q continue to the next letter */
+        continue;
+        putchar(a); /* otherwise print the letter */
+    }
+
+    putchar('\n');
+
 return (0);
 }
