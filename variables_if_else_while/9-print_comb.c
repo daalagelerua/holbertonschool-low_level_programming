3ec8a7a08e23e7072e647fa7614d57f468761129
Aurèle Perillat <aurele284@gmail.com>
2025-01-18 14:52:54 +0100

    add comments

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - entry
+* main - print number 0 to 9 with coma and space
 * Return: 0
 */
 int main(void)
 {
 int a;
-for (a = 0; a <= 9; a++)
-{
-putchar(a + '0');
-if (a < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+    for (a = 0; a <= 9; a++) /* iterate through 0 to 9 */
+    {
+        putchar(a + '0'); /* shift to ascii value and print */
+        if (a < 9) /* if number is smaller than 9 */
+        {
+            putchar(','); /* add a coma and a space */
+            putchar(' ');
+        }
+    }
+    putchar('\n'); /* when 9 is reach just return to new line */
+
+  return (0);
 }
