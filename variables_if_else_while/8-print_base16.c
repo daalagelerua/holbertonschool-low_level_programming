fa6d1fd3e34fc8181b83d78e0a43fade6a185371
Aurèle Perillat <aurele284@gmail.com>
2025-01-18 14:48:08 +0100

    add comments

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 /**
-* main - entry
+* main - print number from 0 to 15 in base 16
 * Return: 0
 */
 int main(void)
 {
 int a;
-for (a = 0; a < 16; a++)
-if (a < 10)
-putchar(a + '0');
-else
-putchar(a - 10 + 'a');
-putchar('\n');
+
+    for (a = 0; a < 16; a++) /* iterate through 0 to 15 */
+        if (a < 10) /* for number 0 to 9 */
+            putchar(a + '0'); /* shift to ascii and print */
+
+        else
+            putchar(a - 10 + 'a'); /* for 10 to 15 remove 10 then add the ascii value of 'a' */
+  
+        putchar('\n');
+
 return (0);
 }
